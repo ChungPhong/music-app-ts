cf29a60d8bde883fcfe54929d0b8798dd170937c
Chung Phong <phonghoang0205@gmail.com>
2024-10-14 00:11:06 +0700

    Lấy ra chủ đề bài hát

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import express, { Express, Request, Response } from "express";
 import * as database from "./config/database";
 import dotenv from "dotenv";
 // import mainV1Routes from "./api/v1/routes/index.route";
+import Topic from "./model/topic.model";
 
 dotenv.config();
 database.connect();
@@ -15,7 +16,12 @@ app.set("view engine", "pug");
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 // mainV1Routes(app);
-app.get("/topics", (req: Request, res: Response) => {
+app.get("/topics", async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    deleted: false,
+  });
+  console.log(topics);
+
   res.render("client/pages/topics/index");
 });
 app.listen(port, () => {
