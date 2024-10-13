6426654474577d75b8e41c777b3202c55b5c5cc4
Chung Phong <phonghoang0205@gmail.com>
2024-10-14 00:18:50 +0700

    Tạo routes

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 // import cors from "cors";
 import * as database from "./config/database";
 import dotenv from "dotenv";
 // import mainV1Routes from "./api/v1/routes/index.route";
-import Topic from "./model/topic.model";
+
+import clientRoutes from "./routes/client/index.route";
 
 dotenv.config();
 database.connect();
@@ -15,15 +16,8 @@ app.set("view engine", "pug");
 // app.use(cors());
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
-// mainV1Routes(app);
-app.get("/topics", async (req: Request, res: Response) => {
-  const topics = await Topic.find({
-    deleted: false,
-  });
-  console.log(topics);
+clientRoutes(app);
 
-  res.render("client/pages/topics/index");
-});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
