ad42283e07e4f6a86f13c4a854713f681b1a07a2
Chung Phong <phonghoang0205@gmail.com>
2024-10-13 23:51:28 +0700

    Kết nối database

--- index.ts
+++ index.ts
@@ -1,17 +1,16 @@
 import express, { Express, Request, Response } from "express";
 // import cors from "cors";
-// import * as database from "./config/database";
-// import dotenv from "dotenv";
+import * as database from "./config/database";
+import dotenv from "dotenv";
 // import mainV1Routes from "./api/v1/routes/index.route";
 
-// dotenv.config();
-// database.connect();
+dotenv.config();
+database.connect();
 const app: Express = express();
-const port: number = 3000;
+const port: number | string = process.env.Port || 3000;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
-// const port: number | string = process.env.Port || 3000;
 // app.use(cors());
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
