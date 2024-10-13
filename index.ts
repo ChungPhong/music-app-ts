9a5e23c93a1d8c6295024fa35bc9b5d22150e069
Chung Phong <phonghoang0205@gmail.com>
2024-10-13 23:24:57 +0700

    Tạo các route cơ bản

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-console.log("ok");
+import express, { Express, Request, Response } from "express";
+// import cors from "cors";
+// import * as database from "./config/database";
+// import dotenv from "dotenv";
+// import mainV1Routes from "./api/v1/routes/index.route";
+
+// dotenv.config();
+// database.connect();
+const app: Express = express();
+const port: number = 3000;
+// const port: number | string = process.env.Port || 3000;
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// mainV1Routes(app);
+app.get("/topics", (req: Request, res: Response) => {
+  res.send("Chủ đề bài hát");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
