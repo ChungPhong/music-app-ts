7843a1573a72a37e936956ffe751a80ff8442a52
Chung Phong <phonghoang0205@gmail.com>
2024-10-15 00:33:47 +0700

    Làm trang danh sách bài hát bên Client

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,9 +1,11 @@
 import { Express } from "express";
 import { topicRoutes } from "./topic.route";
+import { songRoutes } from "./song.route";
 
 // import * as authMiddleware from "../middlewares/auth.middlewares";
 
 const clientRoutes = (app: Express): void => {
   app.use("/topics", topicRoutes);
+  app.use("/songs", songRoutes);
 };
 export default clientRoutes;
