b1557744878ff15a96ccbc1317fc8f734d195238
Chung Phong <phonghoang0205@gmail.com>
2024-10-15 13:54:50 +0700

    Làm tình năng like

--- routes/client/song.route.ts
+++ routes/client/song.route.ts
@@ -4,5 +4,6 @@ import * as controller from "../../controller/client/song.controller";
 
 router.get("/:slugTopic", controller.list);
 router.get("/detail/:slugSong", controller.detail);
+router.patch("/like/:typeLike/:idSong", controller.like);
 
 export const songRoutes: Router = router;
