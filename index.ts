1e0d835226c2382fa8caecd55241736bdf10f125
Chung Phong <phonghoang0205@gmail.com>
2024-10-16 23:16:09 +0700

    Layout chung bên Admin

--- index.ts
+++ index.ts
@@ -5,7 +5,9 @@ import dotenv from "dotenv";
 // import mainV1Routes from "./api/v1/routes/index.route";
 
 import clientRoutes from "./routes/client/index.route";
-
+import adminRoutes from "./routes/admin/index.route";
+import { systemConfig } from "./config/config";
+import path from "path";
 dotenv.config();
 database.connect();
 const app: Express = express();
@@ -15,10 +17,19 @@ app.use(express.static("public"));
 
 app.set("views", "./views");
 app.set("view engine", "pug");
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+
+// TinyMCE
+app.use(
+  "/tinymce",
+  express.static(path.join(__dirname, "node_modules", "tinymce"))
+);
+// End TinyMCE
+
+// App Local Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
 clientRoutes(app);
+adminRoutes(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
